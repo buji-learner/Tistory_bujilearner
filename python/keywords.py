bd6ad161eb25eb6a74a02e4aa04ea67fb38e378f
buji-learn <git.joe627@gmail.com>
2023-10-31 17:28:04 +0900

    keywords assert

---
+++ python/keywords.py
@@ -0,0 +1,12 @@
+# assert
+x = "hello"
+
+#if condition returns True, then nothing happens:
+assert x == "hello"
+
+#if condition returns False, AssertionError is raised:
+# assert x == "goodbye", "x should be hello"
+
+"""
+>>> assert (조건), 에러 메세지
+"""
